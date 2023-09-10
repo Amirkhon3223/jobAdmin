c896f50d8d9a88b4878fdff00cd8aa3c206338f4
Amirich <61590760+Amirkhon3223@users.noreply.github.com>
2023-09-10 20:07:30 +0500

    saving resolve after crash

--- src/app/components/cards/post-vacancy/post-vacancy.component.ts
+++ src/app/components/cards/post-vacancy/post-vacancy.component.ts
@@ -1,96 +1,103 @@
-import { Component } from '@angular/core';
-import {VacancySettingsService} from "../../../services/vacancy-settings.service";
+import { AfterViewInit, Component, ElementRef, Renderer2, ViewChild } from '@angular/core';
+import { VacancySettingsService } from "../../../services/vacancy-settings.service";
 import { FormBuilder, FormGroup, FormArray, Validators } from '@angular/forms';
-import {Vacancy} from "../../../models/vacancy"; // Импортируйте FormBuilder и FormArray
+import { Vacancy } from "../../../models/vacancy";
+import { HttpClient } from "@angular/common/http";
 
 @Component({
   selector: 'app-post-vacancy',
   templateUrl: './post-vacancy.component.html',
   styleUrls: ['./post-vacancy.component.css']
 })
-export class PostVacancyComponent {
-  vacancyForm: FormGroup; // Инициализируйте FormGroup
-  vacancies!: Vacancy[];
-  lists: FormArray; // Добавьте это поле
+export class PostVacancyComponent implements AfterViewInit{
+  @ViewChild('wysiwyg', { static: true }) wysiwyg!: ElementRef;
+
+  vacancyForm: FormGroup;
+  vacancies: Vacancy[] = [];
 
   constructor(
+    private renderer: Renderer2,
+    private elementRef: ElementRef,
     private vacancyService: VacancySettingsService,
-    private fb: FormBuilder // Внедрите FormBuilder
+    private fb: FormBuilder,
+    private http: HttpClient
   ) {
-    this.lists = this.fb.array([]); // Инициализируйте список здесь
-
-    // Инициализация массива vacancies
-    this.vacancies = [];
-
     this.vacancyForm = this.fb.group({
-      id: [0],
       title: ['', Validators.required],
       type: [''],
-      region: [''],
       city: [''],
       employmentType: [''],
-      description: [''],
-      sections: this.fb.array([]),
-      lists: this.lists // Свяжите список с формой
+      description: ['']
     });
   }
 
-  get sections(): FormArray {
-    return this.vacancyForm.get('sections') as FormArray;
+  ngAfterViewInit() {
+    const iframe = this.wysiwyg.nativeElement;
+    const iframeDocument = iframe.contentDocument || iframe.contentWindow?.document;
+
+    if (iframeDocument) {
+      if (iframeDocument.readyState === 'complete') {
+        iframeDocument.designMode = 'on';
+        this.init();
+      } else {
+        iframeDocument.onload = () => {
+          iframeDocument.designMode = 'on';
+          this.init();
+        };
+      }
+    }
   }
 
-  addSection() {
-    const newSection = this.fb.group({
-      type: ['heading'],
-      title: ['', Validators.required],
-      describe: ['']
-    });
-    this.sections.push(newSection);
+  init(): void {
+    const iframe = this.wysiwyg.nativeElement;
+    const iframeDoc = iframe.contentDocument || iframe.contentWindow?.document;
+    iframeDoc.designMode = 'on';
   }
 
-  removeSection(index: number) {
-    this.sections.removeAt(index);
+  formatBold(): void {
+    document.execCommand('bold', false, '');
+  }
+  formatItalic(): void {
+    document.execCommand('italic', false, '');
+  }
+  formatUnderline(): void {
+    document.execCommand('underline', false, '');
+  }
+  formatHeading(): void {
+    const iframe = this.wysiwyg.nativeElement;
+    const iframeDoc = iframe.contentDocument || iframe.contentWindow?.document;
+    const h1 = iframeDoc.createElement('h1');
+    h1.textContent = 'Заголовок';
+    iframeDoc.body.appendChild(h1);
+  }
+  formatBulletList(): void {
+    const iframe = this.wysiwyg.nativeElement;
+    const iframeDoc = iframe.contentDocument || iframe.contentWindow?.document;
+    iframeDoc.execCommand('insertUnorderedList', false, null);
   }
 
-  addList() {
-    const newList = this.fb.group({
-      type: ['list'],
-      title: ['', Validators.required],
-      items: this.fb.array([''], Validators.required)
-    });
-    this.lists.push(newList);
-  }
-
-  removeList(index: number) {
-    this.lists.removeAt(index);
-  }
-
-  removeItemFromList(listIndex: number, itemIndex: number) {
-    const items = this.lists.at(listIndex).get('items') as FormArray;
-    items.removeAt(itemIndex);
-  }
-
-  addItemToList(listIndex: number) {
-    const items = this.lists.at(listIndex).get('items') as FormArray;
-    items.push(this.fb.control('', Validators.required));
-  }
-
-  trackByIndex(index: number, item: any): number {
-    return index;
-  }
 
   addVacancy() {
+    const iframe = this.wysiwyg.nativeElement;
+    const iframeDocument = iframe.contentDocument || iframe.contentWindow?.document;
+
+    if (iframeDocument) {
+      const iframeBody = iframeDocument.body;
+
+      if (iframeBody) {
+        const descriptionValue = iframeBody.innerHTML;
+        this.vacancyForm.get('description')?.setValue(descriptionValue);
+      }
+    }
+
     this.vacancyService.addVacancy(this.vacancyForm.value).subscribe(newVacancy => {
       console.log('Вакансия успешно добавлена:', newVacancy);
-
-      // После успешного добавления, добавьте новую вакансию в начало массива
       this.vacancies.unshift(newVacancy);
-
-      // Очистите форму, если это необходимо
       this.vacancyForm.reset();
     }, error => {
       console.error('Ошибка при добавлении вакансии:', error);
     });
   }
+
 
 }
